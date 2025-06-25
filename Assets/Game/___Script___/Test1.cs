65b01583ad06a08a8ba3d4e92bee4b22e3363154
TonicatFelidae <50703992+TonicatFelidae@users.noreply.github.com>
2025-06-26 03:25:41 +0700

    1.1.10

---
+++ Assets/Game/___Script___/Test1.cs
@@ -0,0 +1,17 @@
+using ET.UIKit.ZenjectUIScreen;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        PopupUI s = new PopupUI();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
